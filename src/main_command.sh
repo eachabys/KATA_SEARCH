c6f168e79d5c5f02a644a2dd9d6cd9871d7fbfa1
eachabys <44489101+eachabys@users.noreply.github.com>
2019-12-02 09:55:59 -0600

    Add files via upload

--- src/main_command.sh
+++ src/main_command.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python3 ./py_scripts/kata_word_search_01.py 
-python3 ./py_scripts/kata_word_search_02.py 
-python3 ./py_scripts/kata_word_search_03.py 
+python3 ./py_scripts/kata_word_search.py 
